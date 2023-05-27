5398ef5806c6a1e6bfa5c695a643f12a78833795
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-05-27 18:46:40 +0900

    Write Base BubbleData class

--- Assets/Scripts/BubbleData.cs
+++ Assets/Scripts/BubbleData.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+//버블의 기본 정보를 저장하는 클래스
+//버블의 색상, 타입 등을 저장하고, 버블 간의 데이터를 교환할 때 사용
 using UnityEngine;
 
-public class BubbleData : MonoBehaviour
+public class BubbleData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Color bubbleColor;
 
-    // Update is called once per frame
-    void Update()
+    public BubbleData(Color bubbleColor)
     {
-        
+        this.bubbleColor = bubbleColor;
     }
 }
