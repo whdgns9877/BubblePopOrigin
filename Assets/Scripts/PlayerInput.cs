22dd0a1bfe9c3b0aca85548aec186291b1bf9572
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-05-29 23:14:53 +0900

    Update PlayerInput.cs
    Add Vector2 touchPos and assign it Convert  ScreenToWorldPoint(Input.touch.position)

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,32 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-// 플레이어의 입력을 감지하고 처리하는 클래스
-// 플레이어의 터치 입력을 받아 해당 위치에 버블을 발사하는 기능을 구현합니다.
-// 입력에 따라 버블의 발사 방향과 속도를 결정합니다.
 
 public enum TouchState { TouchBegan, TouchMoved, TouchEnd }
 
-public class PlayerInput : MonoBehaviour
+public static class PlayerInput
 {
-    public TouchState state;
+    public static TouchState state;
+    public static Vector2 touchPos;
 
-    private void Update()
+    public static void Update()
     {
-        // 터치가 시작되었을 때
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0)
         {
-            state = TouchState.TouchBegan;
-        } // 터치 시작 후 스와이프할 때
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            state = TouchState.TouchMoved;
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                state = TouchState.TouchBegan;
+                touchPos = GetScreenPosition(touch.position);
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                state = TouchState.TouchMoved;
+                touchPos = GetScreenPosition(touch.position);
+            }
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                state = TouchState.TouchEnd;
+            }
         }
-        // 터치가 끝났을 때
-        if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled))
-        {
-            state = TouchState.TouchEnd;
-        }
+    }
+
+    private static Vector2 GetScreenPosition(Vector2 touchPosition)
+    {
+        return Camera.main.ScreenToWorldPoint(touchPosition);
     }
 }
